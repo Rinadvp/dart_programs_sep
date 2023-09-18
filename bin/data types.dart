216cff55896f5063a6912ac8ead00014cd1e3757
Rinadvp <rinadvp@gmail.com>
2023-09-18 12:39:25 +0530

    Initial commit

---
+++ bin/data types.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name='anu';
+  int age   =20;
+  int phone =98262245211;
+  double cgpa=8.1;
+  String email='anu123@gmail,com';
+
+
+  print('my name is $name');
+  print('im $age yrs old');
+  print('my phone number is $phone');
+  print('$cgpa is my cgpa in graduation');
+  print('my mail id is $email');
+
+}
---
+++ bin/first_sample.dart
@@ -0,0 +1,6 @@
+void main(){
+  print('my name is rinad muhammed');
+  print(30);
+  print('my number is 7525545526');
+  print(8.5);
+}
