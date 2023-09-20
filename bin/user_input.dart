fc7be1cfb4f02df7b161b459237d5335eae478b0
Rinadvp <rinadvp@gmail.com>
2023-09-20 12:56:11 +0530

    initial commit

---
+++ bin/user_input.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main(){
+  print('enter your name');
+  String? name = stdin.readLineSync(); // reads a string value
+  print('enter your age');
+  int age = int.parse(stdin.readLineSync()!); // ! - null check
+  print('enter your cgpa');
+  double mark = double.parse(stdin.readLineSync()!);
+
+   // print('name = $name");
+   // print('age = $age');
+   // print('cgpa=$mark');
+
+   stdout.writeln('name : $name');
+   stdout.writeln('age:$age');
+   stdout.writeln('cgpa:$mark');
+   /// stdout.write() _> print all the datas in single line
+  /// stdout.writeln() _> print all the datas in new new line similar to print
+}
---
+++ bin/variables/local_variables_2.dart
@@ -0,0 +1,15 @@
+class myclass{
+  int c =100; //instance variable
+
+ // user defined method
+ void addition(){
+   int a =100,
+       b = 200; /// here a and b are local variables (locally declared variables)
+     print('SUM = ${a+b+c}');
+
+ }
+}
+void main(){
+   myclass obj = myclass();
+   obj.addition();
+}
